82c0f08ccb220dc98c618a4a2bc929afdbfa4b49
Johanna Magdalena <69107842+johannamag@users.noreply.github.com>
2023-11-08 13:43:25 +0100

    added shirt 3d

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,8 +1,91 @@
-export function App() {
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  Center,
+  useGLTF,
+  Environment,
+  AccumulativeShadows,
+  RandomizedLight,
+} from "@react-three/drei";
+import { useRef } from "react";
+import { RawShaderMaterial } from "three";
+import {easing} from 'maath'
+
+export const App = ({ position = [-1, 0, 2.5], fov = 25 }) => (
+  <Canvas
+    shadows
+    eventSource={document.getElementById("root")}
+    eventPrefix="client"
+    camera={{ position, fov }}
+  >
+    <ambientLight intensity={0.5} />
+    <Environment preset="city" />
+    <CameraRig>
+      <Center>
+        <Shirt />
+        <Backdrop />
+      </Center>
+    </CameraRig>
+  </Canvas>
+);
+
+function Shirt(props) {
+  const { nodes, materials } = useGLTF("/shirt_baked_collapsed.glb");
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+      />
+    </group>
+  );
+}
+
+function Backdrop() {
+  return (
+    <AccumulativeShadows
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+    >
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
+        amount={4}
+        radius={5}
+        intensity={0.25}
+        ambient={0.55}
+        position={[-5, -5, -9]}
+      />
+    </AccumulativeShadows>
+  );
+}
+
+function CameraRig({ children }) {
+    const group = useRef()
+    useFrame((state, delta) =>{
+        easing.dampE(
+            group.current.rotation,
+            [state.pointer.y / 8, -state.pointer.x / 3, 0],
+            0.35,
+            delta
+        )
+    })
+
     return (
-        <div className="App">
-            <h1>Hello</h1>
-            <h2>start here</h2>
-        </div>
+        <group ref={group}>
+            {children}
+        </group>
     )
 }
+
+useGLTF.preload("/shirt_baked_collapsed.glb");
